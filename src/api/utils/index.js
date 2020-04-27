21f46fc134f2f5b87dd3debb0f00a3ffcfd5f52e
Chris Michael <chris.perez@upr.edu>
2020-04-27 15:16:33 -0400

    The urlify function has been defined to match the URLs and the getIframeURL function to obtain the URL of the iframe.

--- src/api/utils/index.js
+++ src/api/utils/index.js
@@ -3,6 +3,25 @@ const toInt = (str) => {
   return k;
 };
 
+const urlify = async(text) =>{
+  const urls = [];
+  const urlRegex = /(https?:\/\/[^\s]+)/g;
+  text.replace(urlRegex , (url) =>{
+    urls.push(url)
+  });
+  return Promise.all(urls);
+};
+
+const getIframeURL = async($) =>{
+  const iframe = await $.html().match(/(?:<iframe[^>]*)(?:(?:\/>)|(?:>.*?<\/iframe>))/g)[0];//[0].match(/(?<=src=").*?(?=[\?"])/g)
+  const _url = await urlify(iframe)
+  const url = String(_url[0]);
+
+  return Promise.resolve(url);
+};
+
+
 module.exports = {
   toInt,
+  getIframeURL
 };
